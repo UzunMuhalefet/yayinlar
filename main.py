380c48b55fa4794677aa381482c68f89e1681be2
UzunMuhalefet <80906858+UzunMuhalefet@users.noreply.github.com>
2024-02-03 01:54:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,55 +1,113 @@
-import streamlink, json
-import sys, requests, os
-from urllib.parse import urljoin
+import streamlink
+import sys
+import os 
+import json
 
-def dump(obj):
-  for attr in dir(obj):
-    print("obj.%s = %r" % (attr, getattr(obj, attr)))
+def info_to_text(stream_info, url):
+    text = '#EXT-X-STREAM-INF:'
+    if stream_info.program_id:
+        text = text + 'PROGRAM-ID=' + str(stream_info.program_id) + ','
+    if stream_info.bandwidth:
+        text = text + 'BANDWIDTH=' + str(stream_info.bandwidth) + ','
+    if stream_info.codecs:
+        text = text + 'CODECS="'
+        codecs = stream_info.codecs
+        for i in range(0, len(codecs)):
+            text = text + codecs[i]
+            if len(codecs) - 1 != i:
+                text = text + ','
+        text = text + '",'
+    if stream_info.resolution.width:
+        text = text + 'RESOLUTION=' + str(stream_info.resolution.width) + 'x' + str(stream_info.resolution.height) 
 
-def get_master_playlist(url):
-    try:
-        streams = streamlink.streams(url)
-        return streams["best"].multivariant.uri
-    except:
-        return ""
-
-def get_variant_playlist(url, quality="best"):
-    try:
-        streams = streamlink.streams(url)
-        return streams[quality].url
-    except:
-        return ""
-
+    text = text + "\n" + url + "\n"
+    return text
 
 def main():
+    # Loading config file
     f = open(sys.argv[1], "r")
-    data = json.load(f)
-    #backup_options = data["backup"]
-    #output_type = data["output"]["type"]
-    folder_name = data["output"]["folder"]
+    config = json.load(f)
+
+    # Getting output options and creating folders
+    folder_name = config["output"]["folder"]
+    best_folder_name = config["output"]["bestFolder"]
+    master_folder_name = config["output"]["masterFolder"]
     current_dir = os.getcwd()
-    folder = os.path.join(current_dir, folder_name)
-    os.makedirs(folder, exist_ok=True)
-    channels = data["channels"]
+    root_folder = os.path.join(current_dir, folder_name)
+    best_folder = os.path.join(root_folder, best_folder_name)
+    master_folder = os.path.join(root_folder, master_folder_name)
+    os.makedirs(best_folder, exist_ok=True)
+    os.makedirs(master_folder, exist_ok=True)
+
+    channels = config["channels"]
     for channel in channels:
-        master_playlist = get_master_playlist(channel["url"])
-        if master_playlist:
-            channel_file_path = os.path.join(folder, channel["slug"])
-            channel_file_path = channel_file_path + ".m3u8"
-            r = requests.get(master_playlist)
-            if r.status_code == 200:
-                channel_file = open(channel_file_path, "w+")
-                playlist_text = ""
-                for line in r.iter_lines():
-                    line = line.decode()
-                    if not line.startswith("http") and not line.startswith("#"):
-                        fixed_line = urljoin(r.url, line)
-                        playlist_text += fixed_line
+        # Get streams and playlists
+        try:
+            url = channel["url"]
+            streams = streamlink.streams(url)
+            playlists = streams['best'].multivariant.playlists
+
+            # Text preparation
+            previous_res_height = 0
+            master_text = ''
+            best_text = ''
+
+            # Check http/https options
+            http_flag = False
+            if url.startswith("http://"):
+                plugin_name, plugin_type, given_url  = streamlink.session.Streamlink().resolve_url(url)
+                http_flag = True
+
+            for playlist in playlists:
+                uri = playlist.uri
+                info = playlist.stream_info
+                # Sorting sub-playlist based on 
+                if info.video != "audio_only": 
+                    sub_text = info_to_text(info, uri)
+                    if info.resolution.height > previous_res_height:
+                        master_text = sub_text  + master_text
+                        best_text = sub_text
                     else:
-                        playlist_text += line
-                    playlist_text += "\n"
-                channel_file.write(playlist_text)
+                        master_text = master_text + sub_text
+                    previous_res_height = info.resolution.height
+            
+            # Necessary values for HLS
+            if master_text:
+                if streams['best'].multivariant.version:
+                    master_text = '#EXT-X-VERSION:' + str(streams['best'].multivariant.version) + "\n" + master_text
+                    best_text = '#EXT-X-VERSION:' + str(streams['best'].multivariant.version) + "\n" + best_text
+                master_text = '#EXTM3U\n' + master_text
+                best_text = '#EXTM3U\n' + best_text
+
+            # HTTPS -> HTTP for cinergroup plugin
+            if http_flag:
+                if plugin_name == "cinergroup":
+                    master_text = master_text.replace("https://", "http://")
+                    best_text = best_text.replace("https://", "http://")
+
+            # File operations
+            master_file_path = os.path.join(master_folder, channel["slug"] + ".m3u8")
+            best_file_path = os.path.join(best_folder, channel["slug"] + ".m3u8")
+
+            if master_text:
+                master_file = open(master_file_path, "w+")
+                master_file.write(master_text)
+                master_file.close()
+
+                best_file = open(best_file_path, "w+")
+                best_file.write(best_text)
+                best_file.close()
+                
             else:
-                print("No available streams found for", channel["name"])
+                if os.path.isfile(master_file_path):
+                    os.remove(master_file_path)
+                    os.remove(best_file_path)
+        except Exception as e:
+            master_file_path = os.path.join(master_folder, channel["slug"] + ".m3u8")
+            best_file_path = os.path.join(best_folder, channel["slug"] + ".m3u8")
+            if os.path.isfile(master_file_path):
+                os.remove(master_file_path)
+                os.remove(best_file_path)
+
 if __name__=="__main__": 
     main() 
